f3611d518422b9fc73a58c406d23a9fa97f5a52a
myguelangello <myguelangello624@gmail.com>
2023-06-21 21:00:24 -0300

    chore: adding & config nativewind/tailwindcss

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-zinc-900 flex-1 items-center justify-center'>
+      <Text className='text-zinc-100'>Hello World, I'm Mesa 1</Text>
+      <StatusBar style="light" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
