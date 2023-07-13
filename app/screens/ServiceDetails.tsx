da3271b8b1efd4bb5027d0c08b4a7e935616853c
myguelangello <myguelangello624@gmail.com>
2023-07-13 03:19:33 -0300

    feat: Add functions to apply for service

--- app/screens/ServiceDetails.tsx
+++ app/screens/ServiceDetails.tsx
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Platform,
+  Alert,
 } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 
@@ -16,11 +17,66 @@ import { FetchedServiceProps } from './Services'
 import Title from '../components/Title'
 
 import Icon from '@expo/vector-icons/FontAwesome5'
+import { useEffect, useState } from 'react'
+import * as SecureStore from 'expo-secure-store'
+
 import { convertDate } from '../../src/utils/convert-date-and-time'
+import Button from '../components/Button'
+import { api } from '../../src/lib/api'
 
 export default function ServiceDetails({ route, navigation }) {
   const { bottom, top } = useSafeAreaInsets()
   const { service } = route.params as { service: FetchedServiceProps }
+
+  const [userRole, setUserRole] = useState<string | null>(null)
+  const [userId, setUserId] = useState<number | null>(null)
+
+  useEffect(() => {
+    async function handleAuthenticated() {
+      await SecureStore.getItemAsync('user').then((user) => {
+        setUserRole(JSON.parse(user).role)
+        setUserId(JSON.parse(user).id)
+        console.log('user id =>', userId)
+      })
+    }
+    handleAuthenticated()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  async function handleEnlistment() {
+    let newEnlisted: number[] = []
+    if (service.enlisted.includes(userId)) {
+      Alert.alert('Oops!', `Você já está inscrito para este serviço!`)
+    } else if (service.enlisted.length > 0) {
+      newEnlisted = [...service.enlisted, userId]
+      service.enlisted = newEnlisted
+      handleApplyForService(newEnlisted)
+    } else {
+      newEnlisted = [userId]
+      service.enlisted = newEnlisted
+      handleApplyForService(newEnlisted)
+    }
+  }
+
+  async function handleApplyForService(enlisted: number[]) {
+    try {
+      await api.put(`/api/service-detail/${service.id}/`, {
+        title: service.title,
+        description: service.description,
+        hours: service.hours,
+        hours_value: service.hours_value,
+        contractor: service.contractor,
+        service_date: service.service_date,
+        enlisted,
+      })
+      service.enlisted = enlisted
+
+      Alert.alert('Sucesso!', `Você se candidatou para este serviço!`)
+    } catch (error) {
+      console.error(error)
+      Alert.alert('Ops!', `Não foi possível se candidatar para este serviço!`)
+    }
+  }
 
   return (
     <SafeAreaView className="flex flex-1 bg-brown-400">
@@ -44,12 +100,12 @@ export default function ServiceDetails({ route, navigation }) {
             <TouchableOpacity
               activeOpacity={0.8}
               className="h-10 w-10 items-center justify-center  rounded-full bg-transparent"
-              onPress={() => navigation.goBack()}
+              onPress={() => navigation.navigate('Services')}
             >
               <Icon name="chevron-left" size={24} color="#eaeaea" />
             </TouchableOpacity>
 
-            <Title content="Serviços" className="text-gray-50" />
+            <Title content="Serviços" className="text-zinc-50" />
 
             <TouchableOpacity
               activeOpacity={0.8}
@@ -71,64 +127,81 @@ export default function ServiceDetails({ route, navigation }) {
 
         {/* Description */}
         <View className="h-full bg-zinc-50 px-4">
+          {/* Nº Candidaturas */}
+          <View className="mt-4 flex-row items-center justify-end">
+            <Text className="font-interRegular text-lg leading-tight text-zinc-500">
+              {service?.enlisted.length}
+            </Text>
+            <Text className="ml-1 font-interRegular text-lg leading-tight text-zinc-500">
+              candidaturas
+            </Text>
+          </View>
           {/* Cargo */}
           <View className="mt-4">
-            <Text className="font-interSemiBold text-xl text-gray-700">
+            <Text className="font-interSemiBold text-lg text-zinc-800">
               {service?.title}
             </Text>
-            <Text className="font-interRegular text-base text-gray-400">
+            <Text className="font-interRegular text-base text-zinc-500">
               {service?.description}
             </Text>
           </View>
 
           {/* Local */}
           <View className="mt-4">
-            <Text className="font-interSemiBold text-lg text-gray-700">
+            <Text className="font-interSemiBold text-lg text-zinc-800">
               Endereço
             </Text>
-            <Text className="font-interRegular text-base text-gray-400">
+            <Text className="font-interRegular text-base text-zinc-500">
               {service?.address}
             </Text>
           </View>
 
           {/* Data */}
           <View className="mt-4">
-            <Text className="font-interSemiBold text-lg text-gray-700">
+            <Text className="font-interSemiBold text-lg text-zinc-800">
               Data prevista
             </Text>
-            <Text className="font-interRegular text-base text-gray-400">
+            <Text className="font-interRegular text-base text-zinc-500">
               {convertDate(service?.service_date, 'dddd, D MMMM, YYYY')}
             </Text>
           </View>
 
           {/* Horário */}
           <View className="mt-4">
-            <Text className="font-interSemiBold text-lg text-gray-700">
+            <Text className="font-interSemiBold text-lg text-zinc-800">
               Horário previsto (carga horária)
             </Text>
-            <Text className="font-interRegular text-base text-gray-400">
+            <Text className="font-interRegular text-base text-zinc-500">
               {service?.start_time} ({service?.hours.slice(0, -3)})
             </Text>
           </View>
 
           <View className="mt-4">
-            <Text className="font-interSemiBold text-lg text-gray-700">
+            <Text className="font-interSemiBold text-lg text-zinc-800">
               Valor por hora
             </Text>
-            <Text className="font-interRegular text-base text-gray-400">
+            <Text className="font-interRegular text-base text-zinc-500">
               R$ {service?.hours_value}
             </Text>
           </View>
 
           {/* Candidatura */}
-          <View className="mb-8 mt-8">
-            <Text className="font-interSemiBold text-xl text-gray-400">
-              Candidaturas
-            </Text>
-            <Text className="w-4/5 self-center font-interRegular text-base text-gray-200">
-              No momento não há candidaturas para este serviço
-            </Text>
-          </View>
+          {userRole === '1' ? (
+            <View className="mb-8 mt-8 w-full items-center">
+              <Button title="Candidatar-se" onPress={handleEnlistment} />
+            </View>
+          ) : (
+            <View className="mb-8 mt-8">
+              <Title
+                content="Candidaturas"
+                className="font-interSemiBold text-xl text-zinc-700"
+              />
+
+              <Text className="w-4/5 self-center font-interRegular text-base text-zinc-400">
+                No momento não há candidaturas para este serviço
+              </Text>
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
